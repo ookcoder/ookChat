ab335a568bedac3e209639dc9572dafb94065235
Simon Su <zsu801@aucklanduni.ac.nz>
2020-01-18 14:41:57 +1100

    Setup spring devtools for live reloading

--- src/main/java/com/ook/ookChat/MainView.java
+++ src/main/java/com/ook/ookChat/MainView.java
@@ -1,9 +1,6 @@
 package com.ook.ookChat;
 
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.notification.Notification;
-import org.springframework.beans.factory.annotation.Autowired;
-
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
@@ -12,10 +9,8 @@ import com.vaadin.flow.server.PWA;
 @PWA(name = "Project Base for Vaadin Flow with Spring", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
-    public MainView(@Autowired MessageBean bean) {
-        Button button = new Button("Click me",
-                e -> Notification.show(bean.getMessage()));
-        add(button);
+    public MainView() {
+        add(new H1("Hello"));
     }
 
 }
